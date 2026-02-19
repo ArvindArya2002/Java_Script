6cd43a1e064cf108960c997ac2baee972d499e56
ArvindArya2002 <aryan.singh4198@gmail.com>
2026-02-20 03:19:28 +0530

    Changes done

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -14,4 +14,34 @@ symbol => unique used most in React..
 console.log(typeof "Arvind");
 console.log(typeof null);// object
 console.log(typeof undefined);// undefined
+/*
+    Types of DataTypes
+        a) Primitive
+        7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
 
+        b) Refrence(Non primitive)
+        Array,Objects,Functions
+*/
+// Symbol type Datatype
+const id=Symbol('123')
+const anotherId=Symbol('123')
+// Arrays
+const heros=["shaktiman","naagraj","doga"]
+// Objects
+let myobj= {
+    name: "Arvind",
+    age: 22,
+}
+// Function
+const myFunction=function()
+{
+    console.log("Hello world");
+    
+}
+console.log(typeof Function);
+console.log(typeof Object);
+console.log(typeof Array);
+console.log(typeof BigInt);
+console.log(typeof Number);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01_Basics/04_comparisionOperator.js
@@ -0,0 +1,15 @@
+console.log(+true)
+console.log(+"");
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// === => strictlyChecked(it also checks the datatype of variable)
+
+console.log("2"===2);
+
+
