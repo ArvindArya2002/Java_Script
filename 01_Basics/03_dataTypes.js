2084fd60d68f8eb848f188247d50b6ff41a4e91b
ArvindArya2002 <aryan.singh4198@gmail.com>
2026-02-20 01:45:00 +0530

    Add file

---
+++ 01_Basics/03_dataTypes.js
@@ -0,0 +1,21 @@
+let score="33abc"
+let value=Number(score)
+console.log(typeof value);
+console.log(value);
+/*
+    "33"=33
+    "33abc"=NaN
+    true=1, false=0
+*/
+ let isLoggedIn=""
+ let booleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(typeof booleanIsLoggedIn);
+ console.log(booleanIsLoggedIn);
+ /*
+    1=true, 0=false
+    ""=false
+    "arya"=true
+    33="33"
+*/
+ 
+ 
