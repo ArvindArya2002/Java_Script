d62b6f57804bc322d68ad1dd23497295fd9c9ea9
ArvindArya2002 <aryan.singh4198@gmail.com>
2026-02-19 16:15:32 +0530

    changes done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=12345
+let accountEmail="aryan.singh4198@gmail.com"
+var accountPassword="54321"
+accountCity="Noida"
+/*
+const Var value is not changed..
+prefer not to use var because the problem of block scope or Functional scope
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // it show error
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/False
+null => standalone value
+undefined =>
+symbol => unique used most in React..
+*/
+
+// object
+console.log(typeof "Arvind");
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
+
